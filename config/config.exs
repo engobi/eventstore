1bab6dfca7f585070ab96368f80a61d21ed368ea
Thierry BOMANDOUKI <thierry-bomandouki@ublo.immo>
2026-01-29 11:48:48 +0100

    Update comments

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,9 @@
 import Config
 
-# Configuration globale de EventStore
+# Global configuration of EventStore
 config :eventstore, EventStore,
-  partitioned_events: false,  # Default false, set to true if you want a partioned events table
-  use_pg_partman: false  # Default false, set to true if you want to use postgresql extension pg_partman
+  partitioned_events: false,  # Set to true if you want a partioned events table
+  use_pg_partman: false  # Set to true if you want to use postgresql extension pg_partman
 
 config :eventstore,
   event_stores: [DevEventStore]
